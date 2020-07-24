5b38d43d7732b677b703fe59cd123006870cdb4c
Gyh592358736 <77520082@qq.com>
2020-07-24 03:35:27 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-
+//三静态页面 主页，帮助页，关于页
 Route::get('/', 'StaticPagesController@home')->name('home');;
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');;
+//注册页面
+Route::get('signup', 'UsersController@create')->name('signup');
