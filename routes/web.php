dd85a8d8ad21fb25001ef87756e5d65dbebf479e
Gyh592358736 <77520082@qq.com>
2020-07-24 06:48:33 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');;
 //注册页面
 Route::get('signup', 'UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
