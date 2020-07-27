68a9608f4dba1e1eb804a1410e4c280e19900893
Gyh592358736 <77520082@qq.com>
2020-07-27 01:55:17 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,8 @@ class UsersController extends Controller
     public function create(){
     	return view('users.create');
     }
-    
+
+    //执行注册操作
     public function store(Request $request){
     	$this->validate($request, [
             'name' => 'required|unique:users|max:50',
@@ -34,5 +35,23 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
-    
+    //更新用户
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+    //执行修改操作
+     public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return redirect()->route('users.show', $user->id);
+    }
 }
