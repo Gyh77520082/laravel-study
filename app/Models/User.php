6608edb6c883dc8412d133a39124504dfcc4a898
Gyh592358736 <77520082@qq.com>
2020-08-10 07:02:26 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }    
 }
